41fb2c6501af872588c0ba4afcc9bf36f45c171d
Snagz <96607230+ThatGuySnagz@users.noreply.github.com>
2023-11-03 00:36:35 -0500

    Working somewhat edited generation

--- src/server/room.lua
+++ src/server/room.lua
@@ -1,9 +1,41 @@
 local room = {}
+room.info = require(script.roomInfo)
+room.lastTurnDirection = nil
 room.random = Random.new()
 
-function room.Generate(prevRoom)
+function room.GetRandom(prevRoom)
     local possibleRooms = workspace.Rooms:GetChildren()
     local randomRoom = possibleRooms[room.random:NextInteger(1, #possibleRooms)]
+
+    -- [Rules]
+    -- [1] next room must be different from previous
+    -- [2] if there's a corner then the next turn must turn otherway
+    -- [3] If previous room had stairs, then next room cannot
+
+local direction = room.info[randomRoom.Name]["Direction"]
+local hasStairs = room.info[randomRoom.Name]["Stairs"]
+local prevHadStairs = room.info[prevRoom.Name]["Stairs"]
+
+    if (prevRoom.Name == randomRoom.Name)
+    or (direction and direction == room.lastTurnDirection)
+    or (hasStairs and prevHadStairs)
+    then
+        return room.GetRandom(prevRoom)
+    else
+        if direction then
+        room.lastTurnDirection = direction
+            
+        end
+        return randomRoom
+    end
+
+
+
+end
+
+
+function room.Generate(prevRoom)
+   local randomRoom = room.GetRandom(prevRoom)
     local newRoom = randomRoom:Clone()
 
     newRoom.PrimaryPart = newRoom.Entrance
---
+++ src/server/roominfo.lua
@@ -0,0 +1,29 @@
+local roomInfo = {
+    ["StartRoom"] = {
+        
+    },
+
+    ["Upstairs"] = {
+        ["Stairs"] = true
+    },
+    ["LeftTurn"] = {
+        ["Direction"] = "Left"
+    },
+    ["RightTurn"] = {
+        ["Direction"] = "Right"
+    },
+    ["SmallRoom"] = {
+        
+    },
+    ["LongRoom"] = {
+        
+    },
+    ["LeftSnake"] = {
+        
+    },
+    ["RightSnake"] = {
+        
+    },
+}
+
+return roomInfo
