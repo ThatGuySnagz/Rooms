2038dbae5563d5cc16da112ee917498256e1f3e7
Snagz <96607230+ThatGuySnagz@users.noreply.github.com>
2023-11-02 23:51:18 -0500

    FIRST SUCCESSFUL GENERATION OF MULTIPLE ROOMS

--- src/server/room.lua
+++ src/server/room.lua
@@ -3,15 +3,18 @@ room.random = Random.new()
 
 function room.Generate(prevRoom)
     local possibleRooms = workspace.Rooms:GetChildren()
-    local randomRoom = possibleRooms[room.random:NextINteger(1, #possibleRooms)]
+    local randomRoom = possibleRooms[room.random:NextInteger(1, #possibleRooms)]
     local newRoom = randomRoom:Clone()
 
-    newroom.PrimaryPart = newRoom.Entrance
+    newRoom.PrimaryPart = newRoom.Entrance
     newRoom:PivotTo(prevRoom.Exit.CFrame)
-    newroom.Entrance.Transparency = 1
-    newroom.Exit.Transparency = 1
+    newRoom.Entrance.Transparency = 1
+    newRoom.Exit.Transparency = 1
 
     newRoom.Parent = workspace.GeneratedRooms
+
+    return newRoom
+end
 
 
 return room
